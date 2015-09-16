7aeadd22110227dffc29048e19eefed7b7106f72
Jeremy Eramian <jeremyleramian@outlook.com>
2015-09-16 16:14:04 -0700

    added java feedback file

---
+++ freedback.js
@@ -0,0 +1,5 @@
+var alert;
+function getFeedback() {
+    "use strict";
+    alert("You could be super cool, and drop us some feedback while we work! Checkout the home page and leave your comments in the form!");
+}
