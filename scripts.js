924e78ccbb3c4b95ac0b4b792bb3f173e20b69ce
Jeremy Eramian <jeremyleramian@outlook.com>
2015-09-24 07:14:21 -0700

    fixed function var line

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-var alert;
+var feedBack;
 function getFeedback() {
     "use strict";
     alert("You could be super cool, and drop us some feedback while we work! Checkout the home page and leave your comments in the form!");
